58ab78c7c4782a85ecae5d0811f6a160be99cc8d
Yulia51188 <sun51188@gmail.com>
2018-11-15 09:48:31 +0300

    Парсинг аргументов, проверка существования директории, вывод результата и обработка случая, если дубликаты не найдены

--- duplicates.py
+++ duplicates.py
@@ -1,3 +1,30 @@
+import argparse
+import os
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description='Looking for duplicates in the specified directory.'
+    )
+    parser.add_argument(
+        'directory',
+        type=str,
+        help='directory is need to be checked for duplicates'
+    )
+    return parser.parse_args()
+
+
+def find_duplicates(directory):
+    return None
 
 if __name__ == '__main__':
-    pass
+    args = parse_arguments()
+    if not os.path.exists(args.directory):
+        exit("The specified directory doesn't exist!")
+    duplicates = find_duplicates(args.directory)
+    if duplicates is None:
+        exit('There are no duplicates in the directory')
+    for filename, path_list in duplicates:
+        print('{filename}:'.format(filename))
+        for filepath in path_list:
+            print(filepath)
