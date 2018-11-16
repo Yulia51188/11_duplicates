b6aaa27c1beb619a1849465dfb5989bb338aa180
Yulia51188 <sun51188@gmail.com>
2018-11-16 10:20:38 +0300

    Функция поиска дубликатов без проверки размера

--- duplicates.py
+++ duplicates.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+from collections import Counter
 
 
 def parse_arguments():
@@ -15,6 +16,22 @@ def parse_arguments():
 
 
 def find_duplicates(directory):
+    file_tree = os.walk(directory)
+    filename_dict = {}
+    print(type(filename_dict))
+    for address, dirs, files in file_tree:
+        for filename in files:
+            filepath = os.path.join(address, filename)
+            if filename in filename_dict:
+                filename_dict[filename].append(filepath) 
+            else
+        filename_dict[filename] = filepath
+    duplicate_dict = {}
+    for filename, path_list in filename_dict:
+        if len(path_list) > 1:
+            duplicate_dict[filename] = path_list
+    print(duplicate_dict)
+    
     return None
 
 if __name__ == '__main__':
